ff0d132b1b49cff887e791a2bb732e91ef01a3c1
NPD <DHARANINP341@GMAIL.COM>
2025-08-23 13:54:18 +0530

    Add files via upload

---
+++ Firstprogram.py
@@ -0,0 +1,7 @@
+#how to print an statement in python
+print("Hello,Dk:)")
+#print in 2 diffrent lines
+print("HEY,my name is Dharani")
+print("yoo I have started to code with python.")
+#now to print in the same line
+print("HEY,my name is Dharani,yoo I have started to code with python")
