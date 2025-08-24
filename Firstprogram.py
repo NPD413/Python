9f69e47f169b912d538260b4a2472f64902c4990
NPD <DHARANINP341@GMAIL.COM>
2025-08-24 10:37:01 +0530

    Update Firstprogram.py

--- Firstprogram.py
+++ Firstprogram.py
@@ -1,7 +1,8 @@
 #how to print an statement in python
-print("Hello,Dk:)")
+print("Hello,Dha:)")
 #print in 2 diffrent lines
 print("HEY,my name is Dharani")
 print("yoo I have started to code with python.")
 #now to print in the same line
+
 print("HEY,my name is Dharani,yoo I have started to code with python")
