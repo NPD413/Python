b7e500e333c3c291ea1b86775859796a6fcb4af3
NPD <DHARANINP341@GMAIL.COM>
2025-08-24 10:35:55 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,16 @@
+#variable is basically a name storage that can be stored 
+#inside the memory location it's like the name given to the meomry location to contain the value
+#eg: class(2nd) contains student
+#python is an implicit language which means you dont need to specify the type of the value/data it self understands if it's a string/int/bool etc 
+
+name="npd"
+age=15
+marks=15.777777
+name1="ngch123"
+print(type(name))
+print(type(age))
+print(type(marks))
+print(type(name1))
+
+name="npd"
+print("name") # this way of printing will only print the name not the value which is been stored
