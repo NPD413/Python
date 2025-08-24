722e9fdd8db4d84bf542e32969094b53e77830c7
NPD <DHARANINP341@GMAIL.COM>
2025-08-24 10:40:58 +0530

    Add files via upload

---
+++ arithmetic_operations.py
@@ -0,0 +1,22 @@
+#Arithmetic operations involves someof the basic arthmetc lyk (+,-,%,/,//)etc as well
+a,b=3,5
+sum=3+5
+print(sum) #gives the addition of 2 numbers
+
+c,d=4,5
+e=c%d # it will give the remainder
+f=c/d #it will give the quotient
+print((e),(f))
+
+m,n= 10,10.5
+p=m/n
+q=m//n #division of a interger number eg 0.5 will be given as 0.0
+print((p),(q)) 
+
+t,r=3,2
+s= '@'
+print(t*s*r) #addition of a string
+
+j,h=3,5
+u='#'
+print((j+h)*u) 
