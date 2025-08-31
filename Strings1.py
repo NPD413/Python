ba8941560bed1cd7ce91cfe469167eb4ef6ec82e
NPD <DHARANINP341@GMAIL.COM>
2025-09-01 01:22:12 +0530

    Add files via upload

---
+++ Strings1.py
@@ -0,0 +1,19 @@
+"""strings are datatypes that is used to denote characters """
+str1="Hi welcome to the coading world,this is dharani here"
+print(str1)
+str2="Hi welcome to the coading world.\n this is dharani here"
+print(str2)
+str3="Hi welcome to the coading world,\t this is dharani here"
+print(str3)
+print(str3[1:5])
+#to print the length and the slics function
+str4="this could be us"
+print(len(str4))
+print(str4[0:4])
+print(str3[3:len(str3)])
+str3[1:5]
+#print backwards
+str6="apple"
+print(str6[-5:-2])
+print(str6[-3:-1])
+
